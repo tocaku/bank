9044d2c4330e0be8b43d7ac5495b349545fc2b9a
tocaku <144988619+tocaku@users.noreply.github.com>
2024-02-09 15:04:29 +0200

    Add files via upload

---
+++ readme.txt
@@ -0,0 +1,3 @@
+The application is just a bank to enter the account you need to look at the js file there
+ will be passwords and logins it is an imitation of the bank and just a training application
+ to enter the first letter of the first name and last name in login 
---
+++ script.js
@@ -0,0 +1,395 @@
+'use strict';
+
+// Simply Bank App
+
+const account1 = {
+  userName: 'Cecil Ireland',
+  transactions: [500, 250, -300, 5000, -850, -110, -170, 1100],
+  interest: 1.5,
+  pin: 1111,
+  transactionsDates: [
+    '2020-10-02T14:43:31.074Z',
+    '2020-10-29T11:24:19.761Z',
+    '2020-11-15T10:45:23.907Z',
+    '2021-01-22T12:17:46.255Z',
+    '2021-02-12T15:14:06.486Z',
+    '2021-03-09T11:42:26.371Z',
+    '2021-05-21T07:43:59.331Z',
+    '2021-06-22T15:21:20.814Z',
+  ],
+  currency: 'USD',
+  locale: 'en-US',
+};
+
+const account2 = {
+  userName: 'Amani Salt',
+  transactions: [2000, 6400, -1350, -70, -210, -2000, 5500, -30],
+  interest: 1.3,
+  pin: 2222,
+  transactionsDates: [
+    '2020-10-02T14:43:31.074Z',
+    '2020-10-29T11:24:19.761Z',
+    '2020-11-15T10:45:23.907Z',
+    '2021-01-22T12:17:46.255Z',
+    '2021-02-12T15:14:06.486Z',
+    '2021-03-09T11:42:26.371Z',
+    '2021-05-21T07:43:59.331Z',
+    '2021-06-22T15:21:20.814Z',
+  ],
+  currency: 'UAH',
+  locale: 'uk-UA',
+};
+
+const account3 = {
+  userName: 'Corey Martinez',
+  transactions: [900, -200, 280, 300, -200, 150, 1400, -400],
+  interest: 0.8,
+  pin: 3333,
+  transactionsDates: [
+    '2020-10-02T14:43:31.074Z',
+    '2020-10-29T11:24:19.761Z',
+    '2020-11-15T10:45:23.907Z',
+    '2021-01-22T12:17:46.255Z',
+    '2021-02-12T15:14:06.486Z',
+    '2021-03-09T11:42:26.371Z',
+    '2021-05-21T07:43:59.331Z',
+    '2021-06-22T15:21:20.814Z',
+  ],
+  currency: 'RUB',
+  locale: 'ru-RU',
+};
+
+const account4 = {
+  userName: 'Kamile Searle',
+  transactions: [530, 1300, 500, 40, 190],
+  interest: 1,
+  pin: 4444,
+  transactionsDates: [
+    '2020-10-02T14:43:31.074Z',
+    '2020-10-29T11:24:19.761Z',
+    '2020-11-15T10:45:23.907Z',
+    '2021-01-22T12:17:46.255Z',
+    '2021-02-12T15:14:06.486Z',
+  ],
+  currency: 'EUR',
+  locale: 'fr-CA',
+};
+
+const account5 = {
+  userName: 'Oliver Avila',
+  transactions: [630, 800, 300, 50, 120],
+  interest: 1.1,
+  pin: 5555,
+  transactionsDates: [
+    '2020-10-02T14:43:31.074Z',
+    '2020-10-29T11:24:19.761Z',
+    '2020-11-15T10:45:23.907Z',
+    '2021-01-22T12:17:46.255Z',
+    '2021-02-12T15:14:06.486Z',
+  ],
+  currency: 'USD',
+  locale: 'en-US',
+};
+
+const accounts = [account1, account2, account3, account4, account5];
+
+// Elements
+const labelWelcome = document.querySelector('.welcome');
+const labelDate = document.querySelector('.date');
+const labelBalance = document.querySelector('.balance__value');
+const labelSumIn = document.querySelector('.total__value--in');
+const labelSumOut = document.querySelector('.total__value--out');
+const labelSumInterest = document.querySelector('.total__value--interest');
+const labelTimer = document.querySelector('.timer');
+
+const containerApp = document.querySelector('.app');
+const containerTransactions = document.querySelector('.transactions');
+
+const btnLogin = document.querySelector('.login__btn');
+const btnTransfer = document.querySelector('.form__btn--transfer');
+const btnLoan = document.querySelector('.form__btn--loan');
+const btnClose = document.querySelector('.form__btn--close');
+const btnSort = document.querySelector('.btn--sort');
+
+const inputLoginUsername = document.querySelector('.login__input--user');
+const inputLoginPin = document.querySelector('.login__input--pin');
+const inputTransferTo = document.querySelector('.form__input--to');
+const inputTransferAmount = document.querySelector('.form__input--amount');
+const inputLoanAmount = document.querySelector('.form__input--loan-amount');
+const inputCloseNickname = document.querySelector('.form__input--user');
+const inputClosePin = document.querySelector('.form__input--pin');
+
+const displayTransactions = function (account, transactions, sort = false) {
+  containerTransactions.innerHTML = '';
+  const formatTransactionsDate = function (date, locale) {
+    const getDaystween2Dates = (date1, date2) =>
+      Math.round(Math.abs((date2 - date1) / (1000 * 60 * 60 * 24)));
+
+    const daysPassed = getDaystween2Dates(new Date(), date);
+
+    if (daysPassed === 0) return 'Сегодня';
+    if (daysPassed === 1) return 'Вчера';
+    if (daysPassed <= 7) return `${daysPassed} дней назад`;
+    else {
+      // const day = `${date.getDate()}`.padStart(2, '0');
+      // const month = `${date.getMonth() + 1}`.padStart(2, '0');
+      // const year = date.getFullYear();
+
+      // return `${day}/${month}/${year}`;
+      return new Intl.DateTimeFormat(locale).format(date);
+    }
+  };
+
+  const transacs = sort
+    ? account.transactions.slice().sort((x, y) => x - y)
+    : account.transactions;
+
+  transacs.forEach(function (trans, index) {
+    const transType = trans > 0 ? 'deposit' : 'withdrawal';
+
+    const date = new Date(account.transactionsDates[index]);
+
+    const transDate = formatTransactionsDate(date, account.locale);
+    const formattedTrans = new Intl.NumberFormat(account.locale, {
+      style: 'currency',
+      currency: account.currency,
+    }).format(trans);
+
+    const transactionRow = `
+    <div class="transactions__row">
+      <div class="transactions__type transactions__type--${transType}">
+        ${index + 1} ${transType}
+      </div>
+      <div class="transactions__date">${transDate}</div>
+      <div class="transactions__value">${formattedTrans}</div>
+    </div>
+    `;
+    containerTransactions.insertAdjacentHTML('afterbegin', transactionRow);
+  });
+};
+
+// console.log(containerTransactions.innerHTML);
+
+const createNicknames = function (accs) {
+  accs.forEach(function (acc) {
+    acc.nickname = acc.userName
+      .toLowerCase()
+      .split(' ')
+      .map(word => word[0])
+      .join('');
+  });
+};
+
+createNicknames(accounts);
+// console.log(accounts);
+
+// const userName = 'Oliver Avila'; // nickname = 'oa'
+// const nickname = userName
+//   .toLowerCase()
+//   .split(' ')
+//   .map(word => word[0])
+//   .join('');
+
+// console.log(nickname);
+
+const displayBalance = function (account) {
+  const balance = account.transactions.reduce((acc, trans) => acc + trans, 0);
+  account.balance = balance;
+  labelBalance.textContent = `${balance}$`;
+};
+
+const displayTotal = function (account) {
+  const depositesTotal = account.transactions
+    .filter(trans => trans > 0)
+    .reduce((acc, trans) => acc + trans, 0);
+  labelSumIn.textContent = `${depositesTotal}$`;
+
+  const withdrawalsTotal = account.transactions
+    .filter(trans => trans < 0)
+    .reduce((acc, trans) => acc + trans, 0);
+  labelSumOut.textContent = `${withdrawalsTotal}$`;
+
+  const interestTotal = account.transactions
+    .filter(trans => trans > 0)
+    .map(depos => (depos * account.interest) / 100)
+    .filter((interest, index, arr) => {
+      // console.log(arr);
+      return interest >= 5;
+    })
+    .reduce((acc, interest) => acc + interest, 0);
+  labelSumInterest.textContent = `${interestTotal}$`;
+};
+
+const updateUi = function (account) {
+  // Display transactions
+  displayTransactions(account);
+
+  // Display balance
+  displayBalance(account);
+
+  // Display total
+  displayTotal(account);
+};
+
+let currentAccount, currenLogOutTimer;
+
+// ! постоянно алогинены
+// currentAccount = account1;
+// updateUi(currentAccount);
+// containerApp.style.opacity = 100;
+
+const now = new Date();
+const options = {
+  hour: 'numeric',
+  minute: 'numeric',
+  day: 'numeric',
+  month: 'long',
+  year: 'numeric',
+  weekday: 'long',
+};
+// const locale = navigator.language;
+const startLogoutTimer = function () {
+  const logOutTimerCallback = function () {
+    const minutes = String(Math.trunc(time / 60)).padStart(2, '0');
+    const seconds = String(time % 60).padStart(2, '0');
+    labelTimer.textContent = `${minutes}:${seconds}`;
+    if (time === 0) {
+      clearInterval(lo0utTumer);
+      containerApp.style.opacity = 0;
+      labelWelcome.textContent = 'Войдите в свой аккаунт';
+    }
+
+    time--;
+  };
+
+  let time = 300;
+  logOutTimerCallback();
+
+  const lo0utTumer = setInterval(logOutTimerCallback, 1000);
+
+  return lo0utTumer;
+};
+// Event Handlers
+
+btnLogin.addEventListener('click', function (e) {
+  e.preventDefault();
+  currentAccount = accounts.find(
+    account => account.nickname === inputLoginUsername.value
+  );
+  console.log(currentAccount);
+
+  if (currentAccount?.pin === Number(inputLoginPin.value)) {
+    // Display UI and welcome message
+    containerApp.style.opacity = 100;
+
+    labelWelcome.textContent = `Рады, что вы снова с нами, ${
+      currentAccount.userName.split(' ')[0]
+    }!`;
+    const now = new Date();
+    const day = `${now.getDate()}`.padStart(2, '0');
+    const month = `${now.getMonth() + 1}`.padStart(2, '0');
+    const year = now.getFullYear();
+    labelDate.textContent = `${day}/${month}/${year}`;
+
+    // Clear inputs
+    inputLoginUsername.value = '';
+    inputLoginPin.value = '';
+    inputLoginPin.blur();
+    labelDate.textContent = new Intl.DateTimeFormat(
+      currentAccount.locale,
+      options
+    ).format(now);
+    if (currenLogOutTimer) clearInterval(currenLogOutTimer);
+    currenLogOutTimer = startLogoutTimer();
+
+    updateUi(currentAccount);
+  }
+});
+
+btnTransfer.addEventListener('click', function (e) {
+  e.preventDefault();
+  const transferAmount = Number(inputTransferAmount.value);
+  const recipientNickname = inputTransferTo.value;
+  const recipientAccount = accounts.find(
+    account => account.nickname === recipientNickname
+  );
+  inputTransferTo.value = '';
+  inputTransferAmount.value = '';
+  clearInterval(currenLogOutTimer);
+  currenLogOutTimer = startLogoutTimer();
+  if (
+    transferAmount > 0 &&
+    currentAccount.balance >= transferAmount &&
+    recipientAccount &&
+    currentAccount.nickname !== recipientAccount.nickname
+  ) {
+    currentAccount.transactions.push(-transferAmount);
+    recipientAccount.transactions.push(transferAmount);
+
+    currentAccount.transactionsDates.push(new Date());
+    recipientAccount.transactionsDates.push(new Date());
+    updateUi(currentAccount);
+  }
+});
+
+btnClose.addEventListener('click', function (e) {
+  e.preventDefault();
+  if (
+    inputCloseNickname.value === currentAccount.nickname &&
+    Number(inputClosePin.value) === currentAccount.pin
+  ) {
+    const currentAccountIndex = accounts.findIndex(
+      account => account.nickname === currentAccount.nickname
+    );
+
+    accounts.splice(currentAccountIndex, 1);
+
+    containerApp.style.opacity = 0;
+    labelWelcome.textContent = 'Войдите в свой аккаунт';
+  }
+
+  inputCloseNickname.value = '';
+  inputClosePin.value = '';
+});
+
+btnLoan.addEventListener('click', function (e) {
+  e.preventDefault();
+  const loanAmount = Number(inputLoanAmount.value);
+
+  if (
+    loanAmount > 0 &&
+    currentAccount.transactions.some(trans => trans >= (loanAmount * 10) / 100)
+  ) {
+    setTimeout(function () {
+      currentAccount.transactions.push(loanAmount);
+      currentAccount.transactionsDates.push(new Date());
+      updateUi(currentAccount);
+    }, 5000);
+  }
+  inputLoanAmount.value = '';
+
+  clearInterval(currenLogOutTimer);
+  currenLogOutTimer = startLogoutTimer();
+});
+
+let transactionsSorted = false;
+
+btnSort.addEventListener('click', function (e) {
+  e.preventDefault();
+  displayTransactions(currentAccount, !transactionsSorted);
+  transactionsSorted = !transactionsSorted;
+});
+
+// Array.from() example
+
+// const logoImage = document.querySelector('.logo');
+// logoImage.addEventListener('click', function () {
+//   const transactionsUi = document.querySelectorAll('.transactions__value');
+//   console.log(transactionsUi);
+//   // const transactionsUiArray = Array.from(transactionsUi);
+//   // console.log(transactionsUiArray.map(elem => Number(elem.textContent)));
+//   const transactionsUiArray = Array.from(transactionsUi, elem =>
+//     Number(elem.textContent)
+//   );
+//   console.log(transactionsUiArray);
+// });
